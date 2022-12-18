9f4e1daf166346fb900133964739e1cc6c30c014
tinybunnyotherstory <120902039+tinybunnyotherstory@users.noreply.github.com>
2022-12-18 10:39:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+$(document).ready(function(){
+	$("a[href*='#']").on("click", function(e) {
+		let anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 777);
+		e.preventDefault();
+		return false;
+	});
+});
